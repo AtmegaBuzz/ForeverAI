609b6f5b80ede6b532b39fbd402803e67b201b47
internet_trash <70342528+internettrashh@users.noreply.github.com>
2024-07-10 14:45:50 +0530

    Update Selec.tsx

--- src/components/Selec.tsx
+++ src/components/Selec.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { ConnectButton } from '@arweave-wallet-kit/react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom'; // Import Link from react-router-dom
 import { useAtom } from 'jotai';
 import { isLoadingAtom, loadingMsgAtom } from '@/atoms/globalAtom';
 import { useToast } from './ui/use-toast';
@@ -16,15 +16,14 @@ const Selec: React.FC<SelectProps> = () => {
 
   const AdmissableList =
   {
-    "GPT-2 117M model": "XOJ8FBxa6sGLwChnxhF2L71WkKLSKq1aU5Yn5WnFLrY", // GPT-2 117M model.
-    "GPT-2-XL 4-bit quantized model": "M-OzkyjxWhSvWYF87p0kvmkuAEEkvOzIj4nMNoSIydc", // GPT-2-XL 4-bit quantized model.
-    "Phi-2": "kd34P4974oqZf2Db-hFTUiCipsU6CzbR6t-iJoQhKIo", // Phi-2 
-    "Phi-3 Mini 4k Instruct": "ISrbGzQot05rs_HKC08O_SmkipYQnqgB1yC3mjZZeEo", // Phi-3 Mini 4k Instruct
-    "CodeQwen 1.5 7B Chat q3": "sKqjvBbhqKvgzZT4ojP1FNvt4r_30cqjuIIQIr-3088", // CodeQwen 1.5 7B Chat q3
-    "Llama3 8B Instruct q4": "Pr2YVrxd7VwNdg6ekC0NXWNKXxJbfTlHhhlrKbAd1dA", // Llama3 8B Instruct q4
-    "Llama3 8B Instruct q8": "jbx-H6aq7b3BbNCHlK50Jz9L-6pz9qmldrYXMwjqQVI"  // Llama3 8B Instruct q8
+    "GPT-2 117M model": "XOJ8FBxa6sGLwChnxhF2L71WkKLSKq1aU5Yn5WnFLrY",
+    "GPT-2-XL 4-bit quantized model": "M-OzkyjxWhSvWYF87p0kvmkuAEEkvOzIj4nMNoSIydc",
+    "Phi-2": "kd34P4974oqZf2Db-hFTUiCipsU6CzbR6t-iJoQhKIo",
+    "Phi-3 Mini 4k Instruct": "ISrbGzQot05rs_HKC08O_SmkipYQnqgB1yC3mjZZeEo",
+    "CodeQwen 1.5 7B Chat q3": "sKqjvBbhqKvgzZT4ojP1FNvt4r_30cqjuIIQIr-3088",
+    "Llama3 8B Instruct q4": "Pr2YVrxd7VwNdg6ekC0NXWNKXxJbfTlHhhlrKbAd1dA",
+    "Llama3 8B Instruct q8": "jbx-H6aq7b3BbNCHlK50Jz9L-6pz9qmldrYXMwjqQVI"
   }
-
 
   const navigate = useNavigate();
 
@@ -33,7 +32,6 @@ const Selec: React.FC<SelectProps> = () => {
   };
 
   const navigateNextPage = async () => {
-
     if (selectedLLM) {
       let res = await loadModel();
       if (res == true) {
@@ -42,14 +40,13 @@ const Selec: React.FC<SelectProps> = () => {
     } else {
       toast({
         title: "Select LLM",
-        description: "Please select LLM before porocceding",
+        description: "Please select LLM before proceeding",
         variant: "destructive",
         action: (
           <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
         ),
       })      
     }
-
   };
 
   const entries = Object.entries(AdmissableList).map(([key,]) => (
@@ -57,7 +54,6 @@ const Selec: React.FC<SelectProps> = () => {
   ));
 
   const loadModel = async (): Promise<boolean> => {
-
     setLoading(true);
     setLoadingMsg("Loading LLM into memory...");
 
@@ -84,7 +80,7 @@ const Selec: React.FC<SelectProps> = () => {
       return true
     } else {
       toast({
-        title: "Model failed to Loaded",
+        title: "Model failed to Load",
         description: "Something went wrong",
         variant: "destructive",
         action: (
@@ -94,51 +90,44 @@ const Selec: React.FC<SelectProps> = () => {
 
       return false
     }
-
   };
 
-  // useEffect(() => {
-  //   fetchModels(); // Call fetchModels inside useEffect
-  // }, []);
-
   return (
-    <div className="min-h-screen bg-gradient-to-r from-black to-indigo-900 flex flex-col items-center justify-center text-white">      <header className="w-full flex justify-between items-center p-4">
+    <div className="min-h-screen bg-gradient-to-r from-black to-indigo-900 flex flex-col items-center justify-center text-white">
+      <header className="w-full flex justify-between items-center p-4">
         <div className="flex items-center">
-          <img src="public/logo.svg" alt="Logo" className="mr-2" />
+          <img src="/logo.svg" alt="Logo" className="mr-2" />
           <span className="text-xl font-bold">Malboro.AI</span>
         </div>
         <nav className="flex space-x-4">
-          <a href="/" className="text-white hover:text-zinc-300">Home</a>
-          <a href="/about" className="text-white hover:text-zinc-300">About</a>
+          <Link to="/" className="text-white hover:text-zinc-300">Home</Link> {/* Replace href with Link to */}
+          <Link to="/about" className="text-white hover:text-zinc-300">About</Link> {/* Replace href with Link to */}
           <a href="#" className="translate-y-[-10px] transition-transform">
-  <ConnectButton profileModal={true} showBalance={false} showProfilePicture={true} />
-</a>
+            <ConnectButton profileModal={true} showBalance={false} showProfilePicture={true} />
+          </a>
         </nav>
       </header>
       <main className="flex-grow flex flex-col items-center justify-center">
         <div className="bg-black bg-opacity-50 p-8 rounded-lg shadow-lg w-full max-w-3xl" style={{ backdropFilter: 'blur(10px)' }}>
-        <div className="flex justify-center space-x-4 mb-8">
-          <div className="flex items-center space-x-2">
-            <div className="w-8 h-8 flex items-center justify-center rounded-full border border-primary text-primary">1</div>
-            <div className="w-16 border-t border-primary"></div>
+          <div className="flex justify-center space-x-4 mb-8">
+            <div className="flex items-center space-x-2">
+              <div className="w-8 h-8 flex items-center justify-center rounded-full border border-primary text-primary">1</div>
+              <div className="w-16 border-t border-primary"></div>
+            </div>
+            <div className="flex items-center space-x-2">
+              <div className="w-8 h-8 flex items-center justify-center rounded-full border border-primary text-primary">2</div>
+              <div className="w-16 border-t border-primary"></div>
+            </div>
+            <div className="w-8 h-8 flex items-center justify-center rounded-full border border-primary text-primary">3</div>
           </div>
-          <div className="flex items-center space-x-2">
-            <div className="w-8 h-8 flex items-center justify-center rounded-full border border-primary text-primary">2</div>
-            <div className="w-16 border-t border-primary"></div>
-          </div>
-          <div className="w-8 h-8 flex items-center justify-center rounded-full border border-primary text-primary">3</div>
-        </div>
 
           <h2 className="text-center text-2xl mb-6">Choose a LLM</h2>
           <p className="text-center text-md mb-6">Selected: {selectedLLM}</p>
           <div className="grid grid-cols-2 gap-4">
-
-            {
-              entries
-            }
+            {entries}
           </div>
           <div className="flex justify-end mt-8">
-            <button onClick={() => navigateNextPage()}  className="border border-white p-2 rounded hover:bg-white hover:text-black">
+            <button onClick={() => navigateNextPage()} className="border border-white p-2 rounded hover:bg-white hover:text-black">
               <img src="https://openui.fly.dev/openui/24x24.svg?text=➡" alt="right arrow" />
             </button>
           </div>
