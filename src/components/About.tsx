a6eecaa869658dc670e7f941fa0e57008f161243
AtmegaBuzz <swapnilshinde9382@gmail.com>
2024-07-16 19:43:29 +0530

    fix build

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -60,7 +60,7 @@ function About() {
             </div>
             
             <div className="flex items-center justify-center">
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/eEa5mga_aLE?si=uYRR3pRf6Et2KJVa" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/eEa5mga_aLE?si=uYRR3pRf6Et2KJVa" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
             </div>
 
           </div>
